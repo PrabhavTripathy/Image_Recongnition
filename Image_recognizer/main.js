91978641ae0577a65c7e5c43d14dd53104646aea
PrabhavTripathy <90486566+PrabhavTripathy@users.noreply.github.com>
2021-09-16 20:40:54 -0400

    Add files via upload

--- Image_recognizer/main.js
+++ Image_recognizer/main.js
@@ -10,7 +10,7 @@ Webcam.attach("#camera")
 
 function take_snapshot(){
     Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"'
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>'
 
     });
 }
@@ -18,3 +18,23 @@ function take_snapshot(){
 console.log('ml5 version:', ml5.version);
 
 classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/EdLD3KMYq/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
